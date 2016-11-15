f4625cbb36cf4c88c86b0296291e0bb70a4b73b8
edwinmah <me@edwinmah.com>
2016-11-15 10:59:22 -0500

    Add provider and store

--- js/index.jsx
+++ js/index.jsx
@@ -1,13 +1,15 @@
 require('babel-polyfill');
 var React     = require('react');
 var ReactDOM  = require('react-dom');
-//var Provider  = require('react-redux').Provider;
-//var store     = require('./store');
+var Provider  = require('react-redux').Provider;
+var store     = require('./store');
 var App      = require('./components/App');
 
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('app'));
 });
