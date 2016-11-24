64045560cc0b0066103707cb151d290201617d78
edwinmah <me@edwinmah.com>
2016-11-24 10:00:04 -0500

    Adjust styling for larger screens

--- js/components/footer.jsx
+++ js/components/footer.jsx
@@ -8,7 +8,8 @@ var Footer = React.createClass({
       <footer role="contentinfo" className="site-footer">
         <div className="site-footer__media sm-one-half">
           <img src="assets/images/8475945531_008ecf0cc4_o--medium.jpg"
-            srcSet="assets/images/8475945531_008ecf0cc4_o--large.jpg 819w,
+            srcSet="assets/images/8475945531_008ecf0cc4_o--x-large.jpg 1200w,
+                    assets/images/8475945531_008ecf0cc4_o--large.jpg 819w,
                     assets/images/8475945531_008ecf0cc4_o--medium.jpg 600w"
             sizes="100vw"
             alt="President Obama reviews edits of his second inaugural speech" className="site-footer__img" />
