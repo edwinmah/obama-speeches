2f02481ea1e09243967aa78584f2acc5daad1d0d
edwinmah <me@edwinmah.com>
2017-03-27 15:31:01 -0400

    Use class extends component

--- js/components/loading.jsx
+++ js/components/loading.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
 
-var Loading = React.createClass({
-  render: function() {
+export default class Loading extends React.Component {
+  render() {
     return (
-      <span className="spinner" style={this.props.display}>
+      <span className="spinner">
         <span className="bounce1"></span>
         <span className="bounce2"></span>
         <span className="bounce3"></span>
       </span>
     );
   }
-});
+}
 
-
-module.exports = Loading;
