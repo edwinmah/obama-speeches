3cd6c09fbe8eb91abc115f1d749b972db480c3bd
edwinmah <me@edwinmah.com>
2017-03-27 15:00:01 -0400

    Remove commented class

--- js/components/loading.jsx
+++ js/components/loading.jsx
@@ -1,18 +1,6 @@
 import React from 'react';
 
 
-//export default class Loading extends React.Component {
-//  render() {
-//    return (
-//      <span className="spinner" style={this.props.display}>
-//        <span className="bounce1"></span>
-//        <span className="bounce2"></span>
-//        <span className="bounce3"></span>
-//      </span>
-//    );
-//  }
-//}
-
 var Loading = React.createClass({
   render: function() {
     return (
