e29b253a7341e7edc893be11ecb40c212cd851f2
edwinmah <me@edwinmah.com>
2017-04-08 15:10:38 -0400

    Refactor bounce style

--- js/components/loading.jsx
+++ js/components/loading.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 
 export default class Loading extends React.Component {
-  bounceStyle(animationDelay) {
+  bounceStyle(animationName, animationDelay) {
+    const properties = {
+      duration: '.8s',
+      timing: 'ease-in-out',
+      fillMode: 'both',
+      iterationCount: 'infinite'
+    };
+
     return (
       {
         display: 'inline-block',
@@ -10,51 +17,52 @@ export default class Loading extends React.Component {
         height: '1.125em',
         borderRadius: '100%',
         backgroundColor: '#551413',
-        WebkitAnimation: 'sk-bouncedelay .8s infinite ease-in-out both',
-        animation: 'sk-bouncedelay .8s infinite ease-in-out both',
+        WebkitAnimation: `${animationName} ${properties.duration} ${properties.iterationCount} ${properties.timing} ${properties.fillMode}`,
+        animation: `${animationName} ${properties.duration} ${properties.iterationCount} ${properties.timing} ${properties.fillMode}`,
         WebkitAnimationDelay: `${animationDelay}`,
         animationDelay: `${animationDelay}`
       }
     );
   }
 
-  keyframeRule(property, animationName, scale1, scale2) {
+  transformDeclarations(vendorPrefix, scaleValue) {
     return (
-      `@${property} ${animationName} {
+      `${vendorPrefix}transform: scale(${scaleValue});
+        transform: scale(${scaleValue});`
+    );
+  }
+
+  keyframeRule(vendorPrefix, animationName) {
+    return (
+      `@${vendorPrefix}keyframes ${animationName} {
         0%, 80%, 100% {
-        -webkit-transform: scale(${scale1});
-        transform: scale(${scale1});
+          ${this.transformDeclarations('-webkit-', '.2')}
         } 40% {
-        -webkit-transform: scale(${scale2});
-        transform: scale(${scale2});
+          ${this.transformDeclarations('-webkit-', '.5')}
         }
       }`
     );
   }
 
   render() {
-    const styleSheet = document.styleSheets;
+    const styleSheet    = document.styleSheets;
+    const animationName = 'sk-bouncedelay';
 
     const spinnerStyle = {
       verticalAlign: 'sub',
       margin: '0 auto',
       width: '4.375em',
       textAlign: 'center'
-    }
+    };
 
-    styleSheet[2].insertRule(
-      this.keyframeRule('-webkit-keyframes', 'sk-bouncedelay', '.2', '.5'),
-      styleSheet.length);
-
-    styleSheet[2].insertRule(
-      this.keyframeRule('keyframes', 'sk-bouncedelay', '.2', '.5'),
-      styleSheet.length);
+    styleSheet[2].insertRule(this.keyframeRule('-webkit-', `${animationName}`), styleSheet.length);
+    styleSheet[2].insertRule(this.keyframeRule('', `${animationName}`), styleSheet.length);
 
     return (
       <span className="spinner" style={spinnerStyle}>
-        <span className="bounce1" style={this.bounceStyle('-.32s')}></span>
-        <span className="bounce2" style={this.bounceStyle('-.16s')}></span>
-        <span className="bounce3" style={this.bounceStyle('0s')}></span>
+        <span className="bounce1" style={this.bounceStyle(`${animationName}`, '-.32s')}></span>
+        <span className="bounce2" style={this.bounceStyle(`${animationName}`, '-.16s')}></span>
+        <span className="bounce3" style={this.bounceStyle(`${animationName}`, '0s')}></span>
       </span>
     );
   }
