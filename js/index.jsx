2d5fc866fe62519b089e68505ea87714f0254c36
edwinmah <me@edwinmah.com>
2017-04-11 14:41:46 -0400

    Use browserHistory

--- js/index.jsx
+++ js/index.jsx
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import { store } from './store';
 import App from './components/App';
 import Speech from './components/speech';
@@ -12,7 +12,7 @@ import SpeechList from './components/speech-list';
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Provider store={store}>
-      <Router history={hashHistory}>
+      <Router history={browserHistory}>
         <Route path="" component={App}>
           <Route path="/" component={SpeechList} />
           <Route path="/:id/:slug" component={Speech} />
